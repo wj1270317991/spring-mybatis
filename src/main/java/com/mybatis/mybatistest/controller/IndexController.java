d950abc59f9ff2dd48112d495917b04ebdb9019b
wangjun <1270317991@qq.com>
2019-06-20 22:09:40 +0800

    批量更新

--- src/main/java/com/mybatis/mybatistest/controller/IndexController.java
+++ src/main/java/com/mybatis/mybatistest/controller/IndexController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/mybatis")
 public class IndexController {
@@ -15,10 +18,21 @@ public class IndexController {
     @Autowired
     UserMapper userMapper;
 
-    @GetMapping("/demo1")
+    @GetMapping("/insert")
     public String demo1(){
-        User user = new User(1,"小麦");
+        User user = new User(3,"小麦2");
         int insert = userMapper.insert(user);
         return String.valueOf(insert);
+    }
+
+
+    @GetMapping("/batchUpdate")
+    public String demo2(){
+        List<User> users = new ArrayList<>();
+        users.add(new User(1,"小麦手机1"));
+        users.add(new User(2,"小麦手机2"));
+        users.add(new User(3,"小麦手机3"));
+        int i = userMapper.batchUpdate(users);
+        return "123";
     }
 }
--- src/main/java/com/mybatis/mybatistest/dao/UserMapper.java
+++ src/main/java/com/mybatis/mybatistest/dao/UserMapper.java
@@ -3,6 +3,8 @@ package com.mybatis.mybatistest.dao;
 import com.mybatis.mybatistest.model.User;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 
 @Mapper
 public interface UserMapper {
@@ -10,4 +12,6 @@ public interface UserMapper {
     int insert(User record);
 
     int insertSelective(User record);
+
+    int batchUpdate(List<User> users);
 }
